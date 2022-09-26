95a368729992d467088e105489052b96f11a72fd
HenriqueCamacho <harrycamachofarias@hotmail.com>
2022-09-26 08:21:07 -0400

    Variables in JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,29 @@
-let js = 'amazing';
-alert(js)
+let js = '1';
 console.log(js)
+
+if (js === 1) {
+    console.log("== vs ===")
+}
+else {
+    console.log('nope')
+}
+
+//Cammel case for variables names
+//All uppercase variables --> constant
+//Capital case for Classes
+
+/*for (i = 0; i <= 2; i++) {
+    alert(i)
+}*/
+
+let i = 0
+while (i < 0) {
+    console.log(i)
+    i++
+
+}
+i = 0
+do {
+    console.log(i)
+    i++
+} while (i < 0)
