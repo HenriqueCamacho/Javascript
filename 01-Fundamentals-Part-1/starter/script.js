9e7cedda0310b87d54a41d50ccfa55b87f9f90eb
HenriqueCamacho <harrycamachofarias@hotmail.com>
2022-09-26 22:41:19 -0400

    More about variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,3 +33,54 @@ var Var
 Var = "Never use"
 
 //We don't need to write let const or var, but by not using it will be Global block, declare always
+
+
+//Basic operators
+const ageHenrique = 2030 - 1998;
+console.log('Age in 2030', ageHenrique)
+console.log("My age is " + ageHenrique)
+//+ / ** +=  % x++ x--
+console.log(10 % 6)
+
+//Comparison operators
+//> < >= <= == === !=
+console.log('Henrique older than 24 ' + (ageHenrique > 24))
+const isOlder = ageHenrique > 24
+
+//String and template Literals
+const secondName = "Camacho"
+const job = "Software Engineer"
+const birthYear = 1998
+
+let henrique = "I'm " + firstName + " " + secondName + " and i'm a " + job + ", " + String(2022 - birthYear) + " years old"
+console.log(henrique)
+//Template literals ES6
+henrique = `I'm ${firstName} ${secondName}\nAnd i'm a ${job}  ${String(2022 - birthYear)} years old`
+console.log(henrique)
+const newLine = `Testing my
+Nigga` //don't need \n
+console.log(newLine)
+
+let century //Must be declared outside otherwise we can't acess the variable outside
+if (birthYear <= 2000) {
+    century = 20
+}
+else {
+    century = 21
+}
+console.log(`Century ${century}`)
+
+//Type conversion
+const inputYear = '1991'
+console.log(Number(inputYear), inputYear)
+console.log(Number(inputYear) + 18)
+
+console.log(Number('Jonas')) //Nan failed to convert
+console.log(typeof NaN) //Invalid number
+
+console.log(String(23), 23)
+
+//Type coercion
+//- --> String to Number, + --> Number to String
+console.log("I'm " + 24 + ' years old') //don't need tpo convert explicitly
+console.log("23" - "10" - 3)
