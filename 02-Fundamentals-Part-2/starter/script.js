938019550f023bd510d27c6f90e7a0650ff8a1f3
HenriqueCamacho <harrycamachofarias@hotmail.com>
2022-10-05 15:25:12 -0400

    Arrays and Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -71,3 +71,79 @@ console.log(`Sum of 1++ + 1++ = ${complexOperation(1, 1)}`)
 
 //Functions calling other functions
 
+//Arrays
+const arr = [1, 2, 3, 4, 5]
+const print = (arra) => {
+    arra.forEach((value, index) => {
+        console.log(value)
+        console.log(arra[index])
+    })
+}
+const arr2 = new Array(5, 1, 2, 9, 5)
+console.log(`Number of elements in arr ${arr.length}`)
+//It's const but since array is not primitive value, can be mutated
+//cant arr = [..], but can arr[1] = 2
+
+const arr3 = arr.concat([6, 7]) //--> Not in place
+print(arr3)
+
+const crazyArray = [1, true, 'Michael', [10, 11, 12]]//All sorts of values like Python
+
+//Basic arry methods 
+//END of Array
+arr3.push(8) //--> In place. return the current length
+arr3.pop()//--> In place return the removed element
+
+//Start of the array
+arr3.unshift(0) //Add at the beggining and return the current length
+arr3.shift()//Returns the element at the start
+
+arr3.indexOf(3) // Return the index at wich the element is located
+arr3.includes(5)//Return true if the element is in array
+
+
+//Objects
+const henryArray = { //Mutable, has properties
+    firstName: "Henry Fields",
+    Country: "Brazil",
+    packageSize: 18,
+    birthYear: 1998,
+    HasDriverLicense: true,
+
+
+    CalcAge: function (/*birthYear*/) {
+        this.age = 2022 - this.birthYear
+        return this.age //Arrow functions doesn't work here because of this keyword
+    }
+}
+
+//Dot vs Bracket
+console.log(`Dong size ${henryArray["packageSize"]}cm\n`)
+console.log(`${henryArray["first" + "Name"]}`) //Can receive expression
+
+//Basic object methods
+//Adding
+henryArray.city = "Cuiabá"//Same as henryArray["city"] = "Cuiabá"
+console.log(`Age ${henryArray.CalcAge(/*henryArray.birthYear*/)}`)
+console.log(henryArray.age)
+
+//Loops
+for (let i = 0; i <= 1; i++) {
+    console.log("Looping " + i)
+}
+
+//Typeof arr is an object
+
+let i = 0
+while (i < 4) {
+    if (i === 1) {
+        i++
+        continue //Ignora
+    }
+    console.log("Looping with While " + i)
+    if (i === 2) {
+        break//Don't want 3
+    }
+    i++
+}//Also Do While
+
